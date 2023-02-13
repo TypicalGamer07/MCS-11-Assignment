0e7d1c4356ff354a501ccf38d4bbb5cd4707c5f2
Subhabrata <70436487+TypicalGamer07@users.noreply.github.com>
2023-02-13 14:26:09 +0530

    Create Q2-Answer.c

---
+++ Q2-Answer.c
@@ -0,0 +1,23 @@
+(a) To calculate the power without using the pow(x,y) function of <;math.h>
+
+
+#include <stdio.h>
+// Function to calculate x raised to the power of y
+int power(int x, int y) {
+    int result = 1;
+    int i;
+    for (i = 0; i < y; i++) {
+        result *= x;
+    }
+    return result;
+}
+
+int main() {
+    int x, y;
+    printf("Enter the base number: ");
+    scanf("%d", &x);
+    printf("Enter the exponent: ");
+    scanf("%d", &y);
+    printf("%d raised to the power of %d is: %d\n", x, y, power(x, y));
+    return 0;
+}
